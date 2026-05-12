252882be66c03a6ee45a8a370f10a4e877247216
MicroSwitchers <141078536+MicroSwitchers@users.noreply.github.com>
2026-05-12 00:07:35 -0400

    Add files via upload

---
+++ test_script.js
@@ -0,0 +1,16 @@
+
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        slate: { 950: '#020617' }
+                    },
+                    fontFamily: {
+                        inter: ['Inter', 'sans-serif'],
+                        lexend: ['Lexend', 'sans-serif'],
+                    }
+                }
+            }
+        }
+    
